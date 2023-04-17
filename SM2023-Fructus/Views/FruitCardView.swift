229a541912b03f067129525d1651da2ab69706be
R. Kukuh <rkukuh@gmail.com>
2023-04-18 00:16:54 +0700

    Prepare a card view

---
+++ SM2023-Fructus/Views/FruitCardView.swift
@@ -0,0 +1,33 @@
+//
+//  FruitCardView.swift
+//  SM2023-Fructus
+//
+//  Created by R. Kukuh on 18/04/23.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            // Fruit: Image
+            // Fruit: Title
+            
+            Text("Blueberry")
+            
+            // Fruit: Headline
+            // Button: Start
+            
+        } //: Vstack
+    }
+}
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+    }
+}
