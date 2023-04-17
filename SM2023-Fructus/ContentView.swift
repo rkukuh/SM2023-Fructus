852adcc32ac2d110600da047af460a90d9fd3504
R. Kukuh <rkukuh@gmail.com>
2023-04-17 23:40:58 +0700

    Initial Commit

---
+++ SM2023-Fructus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SM2023-Fructus
+//
+//  Created by R. Kukuh on 17/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SM2023-Fructus/SM2023_FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SM2023_FructusApp.swift
+//  SM2023-Fructus
+//
+//  Created by R. Kukuh on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SM2023_FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
