e7d2f192a812b2de28cf6a1c23ec055f86e22c13
R. Kukuh <rkukuh@gmail.com>
2023-04-20 23:59:56 +0700

    WIP fruit detail view

--- SM2023-Fructus/Views/Fruit List/FruitDetailView.swift
+++ SM2023-Fructus/Views/Fruit List/FruitDetailView.swift
@@ -12,7 +12,33 @@ struct FruitDetailView: View {
     var fruit: Fruit
     
     var body: some View {
-        Text(fruit.title)
+        ScrollView {
+            VStack(alignment: .center) {
+                
+                // Header goes here...
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text(fruit.title)
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(fruit.gradientColors[1])
+                    
+                    Text(fruit.headline)
+                        .font(.headline)
+                    
+                    // Nutrients goes here...
+                    
+                    Text("Learn more about \(fruit.title)".uppercased())
+                        .fontWeight(.bold)
+                        .foregroundColor(fruit.gradientColors[0])
+                    
+                    Text(fruit.description)
+                    
+                } //: VStack
+                .padding(.horizontal, 20)
+                
+            } //: VStack
+        } //: ScroolView
     }
 }
 
